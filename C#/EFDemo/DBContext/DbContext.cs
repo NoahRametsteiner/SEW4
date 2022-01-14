f4fc7362831c55dee6b235f92df5889def71b378
NoahRametsteiner <77678379+NoahRametsteiner@users.noreply.github.com>
2022-01-14 11:55:57 +0100

    SEW Theorie

---
+++ C#/EFDemo/DBContext/DbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDemo.DBContext
+{
+    class DbContext : System.Data.Entity.DbContext
+    {
+
+    }
+}
---
+++ C#/EFDemo/Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDemo.Model
+{
+    class User
+    {
+        public int UserID { set; get; }
+        public string Name { set; get; }
+        public string Email { set; get; }
+    }
+}
