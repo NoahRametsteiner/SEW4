07fd93a993d018b232b600e25638d139336272f6
NoahRametsteiner <77678379+NoahRametsteiner@users.noreply.github.com>
2022-03-09 11:52:46 +0100

    Windows Firewall Tests
    Tested a library that allows me to block programs in the windows firewall. This Program adds and deletes firewall entries.

---
+++ C#/Übung/Windows Firewall/Windows Firewall/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFirewallHelper;
+
+namespace WindowsFirewallHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This Program Needs Root Privileges To Add To The Firewall
+
+            var myRule = FirewallManager.Instance.Rules.SingleOrDefault(r => r.Name == "MyApp Rule");
+            if (myRule != null)
+            {
+                FirewallManager.Instance.Rules.Remove(myRule);
+            }
+            else
+            {
+                var rule = FirewallManager.Instance.CreateApplicationRule(
+                    @"MyApp Rule",
+                    FirewallAction.Block,
+                    @"C:\Users\Notsch\Downloads\vs_Enterprise.exe"
+                );
+                rule.Direction = FirewallDirection.Outbound;
+                FirewallManager.Instance.Rules.Add(rule);
+            }
+        }
+    }
+}
